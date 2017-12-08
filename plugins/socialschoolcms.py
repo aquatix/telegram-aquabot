def5518a34ba2e7056a786e28b1d49e021e6d158
Michiel Scholten <michiel@diginaut.net>
2017-12-08 15:21:20 +0100

    Cache the messages for 25 hours

--- plugins/socialschoolcms.py
+++ plugins/socialschoolcms.py
@@ -1,7 +1,16 @@
-import requests
+import hashlib
+
 import bs4
+import requests
+from pymemcache.client import Client as MemcacheClient
+
+MESSAGE_TTL = 3600 * 25  # Cache messages for 25 hours by default
 
 def get_contents(settings):
+    """Get news items from SocialSchoolCMS for the school defined in settings.py"""
+    # Items get cached in memcached
+    mc = MemcacheClient(('127.0.0.1', 11211))
+
     url = 'http://socialschoolcms.nl/my/website/mededelingen.php?school=' + settings.SOCIALSCHOOLCMS_SCHOOL
 
     response = requests.get(url)
@@ -13,9 +22,16 @@ def get_contents(settings):
             blok_result = ''
             for paragraph in blok.find_all('p'):
                 blok_result = blok_result + str(paragraph).replace('<p>', '').replace('</p>', '\n').strip()
-            messages.append(blok_result)
+
+            blok_hash = hashlib.md5(blok_result.encode('utf-8')).hexdigest()
+            print(blok_hash)
+            if not mc.get(blok_hash):
+                # Only send the message when it was not seen before
+                messages.append(blok_result)
+            # Cache message
+            mc.set(blok_hash, blok_result, MESSAGE_TTL)
 
         return messages
-        result = '\n'.join(messages)
-        return result
-    return url
+
+    # No response 200 received
+    return ['Something went wrong while fetching info']
