61d34892faf4047b8f59967910314c77ec72a3f9
Michiel Scholten <michiel@diginaut.net>
2018-10-17 15:46:04 +0200

    Proper formatting and selection

--- plugins/heemskerkevenementenkalender.py
+++ plugins/heemskerkevenementenkalender.py
@@ -1,11 +1,17 @@
+from datetime import date, timedelta
+
 import bs4
 import requests
 
-from datetime import date, timedelta
+
+def format_calendar_item(item):
+    if item[1]:
+        # Item has a time window
+        return '<b>{}</b> {}: {} (locatie: {})\n'.format(item[0], item[1], item[2], item[3])
+    return '<b>{}</b>: {} (locatie: {})\n'.format(item[0], item[2], item[3])
 
 
 def get_event_items():
-
     url = 'https://www.heemskerk.nl/melden-en-meedoen/evenementenkalender/'
     response = requests.get(url)
 
@@ -24,15 +30,14 @@ def get_event_items():
                         skip_item = True
                     cal_item.append(piece.get_text().strip())
                 if not skip_item:
-                    print(cal_item)
                     cal_items.append(cal_item)
     return cal_items
 
 
-def filter_items_for_date(items, date):
+def filter_items_for_date(items, datestamp):
     result = []
     for item in items:
-        if item[0] == date:
+        if item[0] == datestamp:
             result.append(item)
     return result
 
@@ -43,12 +48,19 @@ def get_items_for_today_and_tomorrow():
     tomorrow = today + timedelta(1)
     today_stamp = '{:02d}-{:02d}-{}'.format(today.day, today.month, today.year)
     tomorrow_stamp = '{:02d}-{:02d}-{}'.format(tomorrow.day, tomorrow.month, tomorrow.year)
-    print(today_stamp)
-    print(tomorrow_stamp)
-    todays_items = filter_items_for_date(today_stamp, all_items)
-    tomorrows_items = filter_items_for_date(tomorrow_stamp, all_items)
+    todays_items = filter_items_for_date(all_items, today_stamp)
+    tomorrows_items = filter_items_for_date(all_items, tomorrow_stamp)
     print(todays_items)
     print(tomorrows_items)
+
+    message = ''
+    for item in todays_items:
+        message += format_calendar_item(item)
+    for item in tomorrows_items:
+        message += format_calendar_item(item)
+
+    print(message)
+    return message
 
 
 if __name__ == '__main__':
